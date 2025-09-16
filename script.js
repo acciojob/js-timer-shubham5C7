933d480cc55ea1ae20ba85a1d87e36b9fe26c9ec
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-16 10:22:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // write js code here if required
+functio setTimer() {
+	const now = new Date();
+
+	const day = String(now.getDate()).padStart(2, '0');
+	const month = String(now.getMonth()+1).padStart(2, '0');
+	const year = now.getFullYear();
+
+	
+	const hour = String(now.getHours()).padStart(2, '0');
+	const min = String(now.getMinutes()).padStart(2, '0');
+	const sec = String(now.getSeconds()).padStart(2, '0');
+	
+	document.getElementById("timer").textContent =
+		 `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+}
+setTimer();
+setInterval(setTimer, 1000); 
